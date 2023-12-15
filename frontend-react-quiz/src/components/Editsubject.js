07b27837fba00be89cfdba5dc2f116d8197748bd
Prakhar <codingbrains8@gmail.com>
2023-12-15 23:27:26 +0530

    test1

--- frontend-react-quiz/src/components/Editsubject.js
+++ frontend-react-quiz/src/components/Editsubject.js
@@ -42,7 +42,7 @@ const Editsubject = () => {
   useEffect(() => {
     fetchSubjects();
   }, []);
-
+//test
   const handleInputChange = (index, name, value) => {
     setUpdateQuestions((prevQuestions) => {
       const updatedQuestions = [...prevQuestions];
